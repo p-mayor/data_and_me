91455fbb4054cae284bffdf839562685b281c8a5
petem14 <petem14@gmail.com>
2019-12-02 13:52:20 -0500

    file upload works, need to add folder upload

--- data_and_me_project/data_and_me_app/admin.py
+++ data_and_me_project/data_and_me_app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from mptt.admin import DraggableMPTTAdmin
 
-# Register your models here.
+from .models import Genre
+
+
+admin.site.register(
+    Genre,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+        # ...more fields if you feel like it...
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
--- data_and_me_project/data_and_me_app/models.py
+++ data_and_me_project/data_and_me_app/models.py
@@ -4,6 +4,10 @@ from mptt.models import MPTTModel, TreeForeignKey
 class Genre(MPTTModel):
     name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    file = models.FileField(blank=True, upload_to='file')
+
+    def __str__(self):
+        return self.name
 
     class MPTTMeta:
         order_insertion_by = ['name']
